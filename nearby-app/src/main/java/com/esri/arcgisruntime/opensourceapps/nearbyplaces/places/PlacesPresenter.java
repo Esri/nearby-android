1a280aa6008e99cbf67c6fff335f29a2a260073b
Puneet Prakash <Puneet_Prakash@esri.com>
2020-11-13 11:56:20 -0800

    change eligible http urls to https

--- nearby-app/src/main/java/com/esri/arcgisruntime/opensourceapps/nearbyplaces/places/PlacesPresenter.java
+++ nearby-app/src/main/java/com/esri/arcgisruntime/opensourceapps/nearbyplaces/places/PlacesPresenter.java
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,7 +44,7 @@ public class PlacesPresenter implements PlacesContract.Presenter {
 
   private LocationService mLocationService;
   private final static int MAX_RESULT_COUNT = 10;
-  private final static String GEOCODE_URL = "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+  private final static String GEOCODE_URL = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
 
   public PlacesPresenter( @NonNull PlacesContract.View listView){
     mPlacesView = listView;
